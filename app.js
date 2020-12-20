5f82ce5438b442c7a19eb89b487edecb35e85bfa
Vedant Karia <vedant.karia@somaiya.edu>
2020-12-20 21:23:48 +0530

    fixed errors

--- app.js
+++ app.js
@@ -3,23 +3,21 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtOutput = document.querySelector("#textOutput")
 var getURL = "https://api.funtranslations.com/translate/shakespeare.json";
 
-function translateURL(txtInput) {
-    return `${getURL}?text=${txtInput}`;
+function translateURL(input) {
+    return `${getURL}?text=${input}`;
 } 
+
 function errorHandler(error) {
     console.log("There is some error", error);
 }
+
 btnTranslate.addEventListener("click", function eventPlay() {
     var inputText = txtInput.value;
-    // console.log(txtInput);
-    // console.log(translateURL(txtInput))
     fetch(translateURL(inputText))
     .then(response => response.json())
     .then(json => {
-        // console.log(json);
-        var result = json;
+        var result = json.contents.translated;
         txtOutput.innerText = result;
-        console.log(result);
     })
     .catch(errorHandler);
 });
